ee16aafea010d1a31a1f16689bb1f6a0410fc6c3
franců <francupatrik@seznam.cz>
2025-11-23 20:31:02 +0100

    config

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -3,7 +3,17 @@ const {test} = require('@playwright/test');
 
 //Toto je struktura playwright testu
 
-test('First Playwright test', async ()=>
+test('Browser Context Playwright test', async ({browser})=>
 {
+    const context = browser.newContext();
+    const page = await context.newPage();
+    await page.goto('https://playwright.dev/');
+
+});
+
+test('Page Playwright test', async ({page})=>
+{
+
+    await page.goto('https://playwright.dev/');
 
 });
